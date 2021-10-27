3a3fd094596a299c24c5855b823bdb39d57772a7
Falin <igor.vanteev@gmail.com>
2021-10-27 17:48:22 +0300

    Initial commit

---
+++ database/migrations/2021_10_27_135304_create_words_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('russian_translation');
+            $table->string('english_translation');
+            $table->string('transcription')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
